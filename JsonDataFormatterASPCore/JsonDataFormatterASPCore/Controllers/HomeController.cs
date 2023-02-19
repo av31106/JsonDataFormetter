cb7ea2b8e9eb48ebc4ad64d51432cd77e73c99c4
Anil Kumar <ipec.anil2@outlook.com>
2023-02-19 18:00:43 +0530

    'core'

---
+++ JsonDataFormatterASPCore/JsonDataFormatterASPCore/Controllers/HomeController.cs
@@ -0,0 +1,56 @@
+﻿using JsonDataFormatterASPCore.Models;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JsonDataFormatterASPCore.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class HomeController : ControllerBaseCustom
+    {
+        [HttpGet]
+        [Route("TestAPI")]
+        public string TestAPI()
+        {
+            return TestMessage;
+        }
+
+        [HttpPost]
+        [Route("FormatJsonData")]
+        public string FormatJsonData([FromForm] string jsonString)
+        {
+            if (!string.IsNullOrEmpty(jsonString))
+            {
+                var jsonObject = JsonConvert.DeserializeObject(jsonString);
+                string jsonStr = JsonConvert.SerializeObject(jsonObject);
+                string jsonFormatted = JValue.Parse(jsonStr).ToString(Formatting.Indented);
+                return jsonFormatted;
+            }
+            else
+            {
+                return "";
+            }
+        }
+        [HttpPost]
+        [Route("RemoveWhiteSpace")]
+        public string RemoveWhiteSpace([FromForm] string jsonString)
+        {
+            if (!string.IsNullOrEmpty(jsonString))
+            {
+                var jsonObject = JsonConvert.DeserializeObject(jsonString);
+                string jsonStr = JsonConvert.SerializeObject(jsonObject);
+                string jsonFormatted = JValue.Parse(jsonStr).ToString(Formatting.None);
+                return jsonFormatted;
+            }
+            else
+            {
+                return "";
+            }
+        }
+    }
+}
---
+++ JsonDataFormatterASPCore/JsonDataFormatterASPCore/Models/JsonInput.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JsonDataFormatterASPCore.Models
+{
+    public class JsonInput
+    {
+        public string JsonString { get; set; }
+    }
+}
